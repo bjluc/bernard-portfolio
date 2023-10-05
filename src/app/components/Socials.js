7f5373263434eccff5272cd78870998cdcaf0c95
Luc Audemard <bjluc@bjluc.co.uk>
2023-10-05 23:28:04 +0100

    about section

--- src/app/components/Socials.js
+++ src/app/components/Socials.js
@@ -1,5 +1,43 @@
-const Socials = () => {
-  return <div>Socials</div>;
-};
+// links
+import Link from 'next/link'
 
-export default Socials;
+// icons
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiTwitterXLine,
+} from 'react-icons/ri'
+
+const Socials = () => {
+  return (
+    <div className='flex items-center gap-x-5 text-lg'>
+      <Link
+        href={''}
+        className='hover:text-accent2 transition-all duration-300'
+      >
+        <RiYoutubeLine />
+      </Link>
+      <Link
+        href={''}
+        className='hover:text-accent2 transition-all duration-300'
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={''}
+        className='hover:text-accent2 transition-all duration-300'
+      >
+        <RiFacebookLine />
+      </Link>
+      <Link
+        href={''}
+        className='hover:text-accent2 transition-all duration-300'
+      >
+        <RiTwitterXLine />
+      </Link>
+    </div>
+  )
+}
+
+export default Socials
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 'use client'
+
 // next image
 import Image from "next/image"
 
@@ -66,7 +67,7 @@ const Home = () => {
           {/* bg image */}
           <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
           {/* particles */}
-          <ParticlesContainer />
+          {/* <ParticlesContainer /> */}
           {/* Avatar */}
           <motion.div
           variants={fadeIn('up', 0.6)} 
